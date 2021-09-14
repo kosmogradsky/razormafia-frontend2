ea0ed3f27b265a087367d982994eaf6e838140a4
Evgenii Kosmogradskii-Ivanov <kosmogradsky@gmail.com>
2021-09-14 12:14:16 +0200

    add mainpage link

--- src/Header.tsx
+++ src/Header.tsx
@@ -19,29 +19,38 @@ export function Header(props: HeaderProps) {
     switch (props.userState.type) {
       case "LoadedUserState": {
         return (
-          <>
+          <div>
             <span>Привет снова, {props.userState.user.email}</span>
             <button type="button" onClick={onSignOut}>
               Выйти
             </button>
-          </>
+          </div>
         );
       }
       case "LoadingUserState": {
-        return <>Загрузка...</>;
+        return <div>Загрузка...</div>;
       }
       case "WithoutUserState": {
         return (
-          <>
-            <Link to="/login" className={styles.loginLink}>
+          <div>
+            <Link to="/login" className={styles.headerLink}>
               Залогиниться
             </Link>
             <Link to="/register">Зарегистрироваться</Link>
-          </>
+          </div>
         );
       }
     }
   }
 
-  return <div className={styles.container}>{getUserView()}</div>;
+  return (
+    <div className={styles.container}>
+      <div>
+        <Link to="/" className={styles.headerLink}>
+          На главную
+        </Link>
+      </div>
+      {getUserView()}
+    </div>
+  );
 }
